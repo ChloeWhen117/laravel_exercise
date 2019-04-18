289875b202524b9bc9e51f4642841829a7453bca
chloewhen117 <chloewhen21@gmail.com>
2019-04-17 19:48:04 -0700

    setup views

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'Contact')
+  <h1>Contact</h1>
+
+  <p>Alas there is no contact form here.</p>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('content')
+<h1>Home</h1>
+
+</html>
+    <p>Welcome home!</p>
+
+    <p>Why not <a href="/contact">contact us</a>?</p>
+
+@endsection
