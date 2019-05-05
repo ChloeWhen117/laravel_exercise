dcba0eaa1bb833402df684694b68295b211572d3
chloewhen117 <chloewhen21@gmail.com>
2019-05-05 14:52:26 -0700

    Readjusted style for contacts page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -3,9 +3,18 @@
 @section('title', 'Contact')
 
 @section('content')
+<div style="display: inline;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <hr />
+                <h1 class="title">Contact</h1>
+                <hr />
 
-    <h1>Contact</h1>
-
-    <p>Alas there is no contact form here.</p>
+                 <p>Alas there is no contact form here.</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
