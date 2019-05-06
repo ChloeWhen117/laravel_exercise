e572aa1712fd82068b96345b7e4d14f94276ef6d
chloewhen117 <chloewhen21@gmail.com>
2019-05-05 20:25:32 -0700

    Completed email notification when user creates new Project

--- app/Mail/ProjectCreated.php
+++ app/Mail/ProjectCreated.php
@@ -11,12 +11,14 @@ class ProjectCreated extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $project;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($project)
     {
         $this->project = $project;
     }
--- app/Project.php
+++ app/Project.php
@@ -2,11 +2,16 @@
 
 namespace App;
 
+use App\Events\ProjectCreated;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
     protected $guarded = [];
+
+    protected $dispatchesEvents = [
+        'created' => ProjectCreated::class
+    ];
 
     public function tasks()
     {
@@ -16,5 +21,10 @@ class Project extends Model
     public function addTask($attributes)
     {
         return $this->tasks()->create($attributes);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'author_id');
     }
 }
